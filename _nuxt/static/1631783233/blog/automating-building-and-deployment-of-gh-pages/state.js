6e6fc32a70f92cc6b9092555df8256a55c0f693a
DET171 <DET171@users.noreply.github.com>
2021-09-16 09:07:15 +0000

    deploy: 7c6c2c25dc6fb1557fe603a4e58ac16b484b1cd1

---
+++ _nuxt/static/1631783233/blog/automating-building-and-deployment-of-gh-pages/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631783233",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fautomating-building-and-deployment-of-gh-pages",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aea2f0a4"}}}}(null));
