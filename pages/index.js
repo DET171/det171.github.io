7c949c5f158bdd807723d1873feebeb1bd919e92
DET171 <mrgeek484@gmail.com>
2022-09-09 17:01:33 +0800

    disnots

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Home() {
+	useEffect(() => {
+		AOS.init({
+			duration: 1000,
+		});
+	}, []);
+
 	return (
-		<div className='bg-slate-200'>
+		<div className='bg-slate-100'>
 			<Head>
 				<title>Home</title>
 			</Head>
-			<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+			<div className='flex flex-col items-center justify-center min-h-screen pt-8 pb-3'>
 				<main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
 					{/* eslint-disable-next-line @next/next/no-img-element */}
 					<img
@@ -18,8 +25,29 @@ export default function Home() {
 					/>
 					<h1 className='text-6xl font-bold italic py-10'>Canaris</h1>
 					<p className='mt-3'>
-						I see you have found my personal space on the web. This page is currently WIP, maybe come back sometime later?
+						I see you have found my personal space on the web.
 					</p>
+
+					<div
+						data-aos='fade-up'
+						className='p-10 rounded-lg items-center max-w-4xl mt-10 sm:w-full bg-slate-300 shadow-2xl'
+					>
+						<h4 className='text-3xl font-bold italic mb-4 text-center'>About Me</h4>
+						<p className='my-5 text-left'>
+							I am a student in High School from Singapore. I like programming in my free time (which unfortunately I don&apos;t have much of). I mostly use JavaScript/TypeScript for backend and frontend development. I&apos;m currently learning Rust and also interested in game development although I haven&apos;t had time to learn it yet.
+						</p>
+					</div>
+
+					<div
+						data-aos='fade-left'
+						className='p-10 rounded-lg items-center max-w-4xl mt-10 sm:w-full bg-slate-300 shadow-2xl'
+					>
+						<h4 className='text-3xl font-bold italic mb-4 text-center'>Other things I do</h4>
+						<p className='my-5 text-left'>
+							As with most people, I like playing games during my (occasional) free time. I mostly sell my soul to Gaijin and grind War Thunder. A few other ones I occasionally touch are Phantom Forces and Mincraft. <br />
+							And yes, I main Germany and USA.
+						</p>
+					</div>
 				</main>
 			</div>
 
