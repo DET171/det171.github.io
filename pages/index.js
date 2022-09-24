be7aca2343890290ec604c24ef09b31308edb97c
DET171 <mrgeek484@gmail.com>
2022-09-24 22:14:54 +0800

    yes

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,9 @@ export default function Home({ posts }) {
 						className='inline object-cover w-60 h-60 mr-2 rounded-full'
 						src='https://avatars.githubusercontent.com/u/68373699?v=4'
 					/>
-					<h1 className='text-6xl font-display italic py-10'>Hi, I am Canaris</h1>
+					<h1 className='text-6xl font-display italic py-10'>Hi, I am Canaris,</h1>
 					<p className='text-2xl font-display'>
-						I am a <span class='description'></span>
+						a <span class='description'></span>
 					</p>
 
 					<div
