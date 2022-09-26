5c18cef1419e492d131790479fbad1f74a5fce8c
DET171 <mrgeek484@gmail.com>
2022-09-26 16:35:11 +0800

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GoogleAnalytics trackPageViews gaMeasurementId='G-4GPB1Z1HL6' />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
