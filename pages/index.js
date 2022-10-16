911d1d674c565cfa1cfe2aeda59ab6018668bde7
Canaris <mrgeek484@gmail.com>
2022-10-16 10:18:59 +0800

    e

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
-export default function Home({ posts }) {
+export default function Home() {
 	useEffect(() => {
 		AOS.init({
 			duration: 1000,
