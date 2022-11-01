cc79b3d4df8ff6738783e4b907648ee5ef756cc2
Canaris <mrgeek484@gmail.com>
2022-11-01 17:35:53 +0800

    breh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	important: true,
 	content: [
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
