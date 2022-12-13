317907154049cb53f47f68ee8825171322a83020
Canaris <mrgeek484@gmail.com>
2022-12-13 16:49:32 +0800

    a

--- app/daily-shitposts/page.js
+++ app/daily-shitposts/page.js
@@ -1,4 +1,12 @@
 import fs from 'fs/promises';
+import MarkdownIt from 'markdown-it';
+import shiki from 'shiki';
+const md = new MarkdownIt({
+	html: true,
+	linkify: true,
+	typographer: true,
+});
+
 
 export default async function App() {
 	const statuses = [];
@@ -7,7 +15,7 @@ export default async function App() {
 		const content = await fs.readFile(`${process.cwd()}/app/daily-shitposts/posts/${file}`, 'utf-8');
 		statuses.push({
 			date: file.split('.')[0],
-			content,
+			content: md.render(content),
 		});
 	}
 
@@ -25,7 +33,9 @@ export default async function App() {
 					.map((status) => (
 						<div key={status.date} className='duration-300 p-10 items-center max-w-4xl mt-10 sm:w-full bg-zinc-300 shadow-2xl w-full hover:shadow-[0_45px_80px_-20px_rgba(0,0,0,0.3)]'>
 							<h4 className='text-3xl font-bold italic mb-4 text-center'>{status.date}</h4>
-							<p className='my-5 text-left'>{status.content}</p>
+							<div className='my-5 text-left prose w-full max-w-none' dangerouslySetInnerHTML={{
+								__html: status.content,
+							}}></div>
 						</div>
 					))}
 			</div>
