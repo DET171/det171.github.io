57b7e5f2701062c9c7753874a8b6f8d7fae472f7
Canaris <mrgeek484@gmail.com>
2022-12-14 14:33:07 +0800

    a

--- scripts/createPost.js
+++ scripts/createPost.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const console = require('consola');
 
 const date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
 
-fs.writeFileSync(path.join(__dirname, '../app/daily-shitposts/posts/', `${date}.mdx`), `---
-date: ${date}
----
-`);
+fs.writeFileSync(path.join(__dirname, '../app/daily-shitposts/posts/', `${date}.mdx`), '');
 
+console.success(chalk.green(`Created post for ${date}!`));
