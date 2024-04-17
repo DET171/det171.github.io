bd9dd6ecb55d2315167862829381a97c4cfb93c1
Canaris <mrgeek484@gmail.com>
2024-04-17 20:10:09 +0800

    a

--- astro.config.ts
+++ astro.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: 'https://astro-cactus.chriswilliams.dev/',
+	site: 'https://canaris.is-a.dev',
 	vite: {
 		optimizeDeps: {
 			exclude: ['@resvg/resvg-js'],
