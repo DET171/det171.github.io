31580485efc24ed0671ad5cb1f45264652ffe61c
Canaris <mrgeek484@gmail.com>
2025-06-04 21:59:08 +0800

    update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://fuwari.vercel.app/',
+	site: 'https://canaris.is-a.dev/',
 	base: '/',
 	trailingSlash: 'always',
 	integrations: [
